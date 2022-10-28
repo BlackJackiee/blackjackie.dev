66ac01fe250d75d06e6e036e56aed82a1a5c5a71
BlackJackiee <88632829+BlackJackiee@users.noreply.github.com>
2022-10-28 21:01:34 +0100

    Slight Re-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -11,24 +11,25 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
+        <a
+          href="https://www.roblox.com/users/214354501/profile"
+          target="_blank"
+        >
+          <img src="/Monke.png" className="logo Monke" alt="BlackJackie logo" />
+        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + BlackJackie + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p>My Site Is Currently Being Built Up Using Vite+React+Bootstrap!</p>
+        <p className="read-the-docs">
+          Check In Later To Check If It's Done Yet!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
